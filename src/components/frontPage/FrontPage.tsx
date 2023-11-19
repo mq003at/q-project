dfab56287b160e9f58cc8031064404ab3a8fafb1
Quan Nguyen <mq003at@gmail.com>
2023-11-19 23:49:09 +0200

    update scss

--- src/components/frontPage/FrontPage.tsx
+++ src/components/frontPage/FrontPage.tsx
@@ -1,12 +1,16 @@
 
 
 import { Box } from "@mui/material";
+import Avatar from "../../assets/avatar.jpg";
 
 const FrontPage: React.FC = () => {
     
     return(
         <Box className="frontpage cover">
             <Box className="home">
+                <Box className="img-div">
+                    <Box className="img-avatar" component={"img"} alt="Default Avatar" src={Avatar}></Box>
+                </Box>
                 <Box className="say-hi">HI THERE! MY NAME IS</Box>
                 <Box className="my-name">QUAN NGUYEN</Box>
                 <Box className="a">A</Box>
