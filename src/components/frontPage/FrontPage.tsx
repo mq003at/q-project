414ef4fd476c0b188ae1eb0e2b68ce92d8ddc833
Quan Nguyen <mq003at@gmail.com>
2023-12-19 02:18:24 +0200

    add animation to texts

--- src/components/frontPage/FrontPage.tsx
+++ src/components/frontPage/FrontPage.tsx
@@ -1,21 +1,75 @@
 
 
-import { Box } from "@mui/material";
-import Avatar from "../../assets/avatar.jpg";
+import { Box, Button } from "@mui/material";
+import Avatar from "../../assets/avatar-2500.jpg";
+import Typewriter from "react-ts-typewriter";
+import { useEffect, useState } from "react";
+
+import { LinkedIn, GitHub, Email } from '@mui/icons-material/';
 
 const FrontPage: React.FC = () => {
-    
-    return(
+    const randomAdj: string[] = ["PASSIONATE", "ENTHUSIASTIC", "DEVOTED", "COMITTED", "DEDICATED", "DYNAMIC"]
+    const [chosenAdj, setChosenAdj] = useState<[string, number]>(["PASSIONATE", 0]);
+
+    const [isHovered, setIsHovered] = useState<boolean>(false);
+    const [count, setCount] = useState<number>(1);
+    const [toggle, setToggle] = useState<boolean>(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+        setCount(count + 1);
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    }
+
+    const replaceAdj = () => {
+        let rI: number;
+        do {
+            rI = Math.floor(Math.random() * randomAdj.length);
+        } while (rI === chosenAdj[1])
+
+        setToggle(true);
+
+        setTimeout(() => {
+            setChosenAdj([randomAdj[rI], rI]);
+            setToggle(false);
+        }, 1000);
+    }
+
+    return (
         <Box className="frontpage cover">
-            <Box className="home">
+            <Box className="home section">
                 <Box className="img-div">
-                    <Box className="img-avatar" component={"img"} alt="Default Avatar" src={Avatar}></Box>
+                    <Box className="img-avatar" style={{ transform: isHovered ? `rotate(${count * 360}deg)` : `rotate(${count * 360}deg)` }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} component={"img"} alt="Default Avatar" src={Avatar}></Box>
                 </Box>
-                <Box className="say-hi">HI THERE! MY NAME IS</Box>
-                <Box className="my-name">QUAN NGUYEN</Box>
-                <Box className="a">A</Box>
-                <Box className="title">FULLSTACK DEVELOPER</Box>
-                <Box className="who">who passionate about creating interactive applications and experiences on the web.</Box>
+                <Box className="text-div">
+                    <Box className="say-hi">HI THERE! MY NAME IS</Box>
+                    <Box className="my-name">QUAN NGUYEN</Box>
+                    <Box className="passionate">
+                        <span>YOUR</span>
+                        <Box className={`adjective ${toggle}`} onClick={() => replaceAdj()}>{chosenAdj[0]}</Box>
+                    </Box>
+                    <Box className="title">FULLSTACK DEVELOPER</Box>
+                    <Box className="who">
+                        <div className="on">ON HIS </div>
+                        <div className="text-wrapper">
+                            <span className="journey-1">PATH TO BECOME A GREAT DEVELOPER</span>
+                            <span className="journey-2">JOURNEY IN THE WORLD OF TECHNOLOGY</span>
+                            <span className="journey-3">EXPLORATION OF THE MACHINE'S POTENTIAL</span>
+                            <span className="journey-4">ADVENTURE IN THE CODING REALM</span>
+                        </div>
+                    </Box>
+                    <Box className="direct-wrapper">
+                        <Button className="resume">Resume</Button>
+                        <Box className="other-direct">
+                            <LinkedIn className="icon" />
+                            <GitHub className="icon" />
+                            <Email className="icon" />
+                        </Box>
+                    </Box>
+                </Box>
             </Box>
         </Box>
     )
