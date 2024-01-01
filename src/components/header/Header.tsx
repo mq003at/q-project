3965b4ff4831e5ec3fa8f2182f430ab38a807191
Quan Nguyen <mq003at@gmail.com>
2024-01-01 17:58:02 +0200

    Add button (break CSS -> need header changed)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,44 +1,53 @@
-import { AppBar, Box, Container, Toolbar } from '@mui/material'
+import { AppBar, Box, Button, Container, Toolbar } from '@mui/material'
 import logo from '../../assets/Q-logo-letter-green.png'
 import HeaderBtn from './HeaderBtn'
 
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
+import { HomeMiniOutlined, HomeOutlined, Menu } from '@mui/icons-material/'
+import { Fragment } from 'react'
 
 export const Header: React.FC = () => {
   return (
-    <Box className='header wrapper'>
-      <AppBar className='header appbar' position='fixed'>
-        <Container className='container'>
-          <Toolbar
-            className='toolbar'
-            sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' }}
-          >
-            <Box
-              className='main-logo wrapper'
-              sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+    <Fragment>
+      {/* Toggle for the header */}
+      <Button variant="contained" className='header-toggler'>
+        <Menu />
+      </Button>
+
+      {/* Main header content */}
+      <Box className='header wrapper'>
+        <AppBar className='header appbar' position='fixed'>
+          <Container className='container'>
+            <Toolbar
+              className='toolbar'
+              sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' }}
             >
-              <img src={logo} alt='placeholder-logo' style={{ maxWidth: '100%', maxHeight: '100%' }} />
-            </Box>
-            <Box className='header-buttons'>
-              <HeaderBtn section='home' icon={<HomeOutlinedIcon />} offset={'0'}>
-                Home
-              </HeaderBtn>
-              <HeaderBtn section='about' icon={<HomeOutlinedIcon />} offset={'200'}>
-                About Me
-              </HeaderBtn>
-              <HeaderBtn section='skills' icon={<HomeOutlinedIcon />} offset={'0'}>
-                Skills
-              </HeaderBtn>
-              <HeaderBtn section='projects' icon={<HomeOutlinedIcon />} offset={'0'}>
-                Projects
-              </HeaderBtn>
-              <HeaderBtn section='contact-me' icon={<HomeOutlinedIcon />} offset={'0'}>
-                Contact
-              </HeaderBtn>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </Box>
+              <Box
+                className='main-logo wrapper'
+                sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+              >
+                <img src={logo} alt='placeholder-logo' style={{ maxWidth: '100%', maxHeight: '100%' }} />
+              </Box>
+              <Box className='header-buttons'>
+                <HeaderBtn section='home' icon={<HomeOutlined />} offset={'0'}>
+                  Home
+                </HeaderBtn>
+                <HeaderBtn section='about' icon={<HomeOutlined />} offset={'200'}>
+                  About Me
+                </HeaderBtn>
+                <HeaderBtn section='skills' icon={<HomeOutlined />} offset={'0'}>
+                  Skills
+                </HeaderBtn>
+                <HeaderBtn section='projects' icon={<HomeOutlined />} offset={'0'}>
+                  Projects
+                </HeaderBtn>
+                <HeaderBtn section='contact-me' icon={<HomeOutlined />} offset={'0'}>
+                  Contact
+                </HeaderBtn>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </Box>
+    </Fragment>
   )
 }
